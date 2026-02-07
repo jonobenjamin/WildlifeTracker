f85228883f8c711b577c287c8daf63536c00d544
jonobenjamin <163100744+jonobenjamin@users.noreply.github.com>
2026-02-07 17:06:58 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -101,7 +101,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-api-key, x-admin-key');
   res.header('Access-Control-Allow-Credentials', 'true');
 
